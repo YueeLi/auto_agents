f15ca4af6e6dcfce8aed16bbd8f22b2bfcbbbf02
YueeLi <lcyubuntu@gmail.com>
2025-06-24 22:58:24 +0800

    创建 defer.py

---
+++ src/graph/defer.py
@@ -0,0 +1,58 @@
+"""
+当您希望将某个节点的执行延迟到所有其他待处理任务完成时，延迟节点的执行非常有用。
+当分支长度不同时，延迟节点的执行尤其重要，这种情况在 Map-Reduce 流程等工作流中很常见。
+
+"""
+
+import operator
+from typing import Annotated, Any
+
+from typing_extensions import TypedDict
+
+from langgraph.graph import StateGraph, START, END
+
+
+class State(TypedDict):
+    # The operator.add reducer fn makes this append-only
+    aggregate: Annotated[list, operator.add]
+
+
+def a(state: State):
+    print(f'Adding "A" to {state["aggregate"]}')
+    return {"aggregate": ["A"]}
+
+
+def b(state: State):
+    print(f'Adding "B" to {state["aggregate"]}')
+    return {"aggregate": ["B"]}
+
+
+def b_2(state: State):
+    print(f'Adding "B_2" to {state["aggregate"]}')
+    return {"aggregate": ["B_2"]}
+
+
+def c(state: State):
+    print(f'Adding "C" to {state["aggregate"]}')
+    return {"aggregate": ["C"]}
+
+
+def d(state: State):
+    print(f'Adding "D" to {state["aggregate"]}')
+    return {"aggregate": ["D"]}
+
+
+builder = StateGraph(State)
+builder.add_node(a)
+builder.add_node(b)
+builder.add_node(b_2)
+builder.add_node(c)
+builder.add_node(d, defer=True)
+builder.add_edge(START, "a")
+builder.add_edge("a", "b")
+builder.add_edge("a", "c")
+builder.add_edge("b", "b_2")
+builder.add_edge("b_2", "d")
+builder.add_edge("c", "d")
+builder.add_edge("d", END)
+graph = builder.compile()
